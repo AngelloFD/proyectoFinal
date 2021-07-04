b2d0e3decfdb6930fe8bb3c6f423cdf20627c600
Aldoju <86484822+Aldoju@users.noreply.github.com>
2021-07-03 23:25:32 -0500

    Cambios en el menu
    Habian opciones en el menu, en las que el personaje no se daba cuenta de la mochila y las siguientes opciones mencionaban la mochila.

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ print(
 empezar_g = input("¿Desea continuar? (N/S): ").upper()
 mochilaOp = True
 if empezar_g != 'S':
-    print("El frio te inmoviliza, hiela tus piernas y esperas el final con una ultima vista a la oscuridad")
+    print("El frio te inmoviliza, hiela tus piernas y esperas el final con una última vista a la oscuridad")
     dibujarMonstro()
     time.sleep(3)
     sys.exit()
@@ -68,11 +68,12 @@ else:
     if elecc == "A":
         print("Miras a tu alrededor y solo vez oscuridad.\n"
               "El suelo en donde caíste era duro y accidentado.\n")
-        print("No hay alguna luz más de la que cae de la luna a travez del hueco de donde caíste.")
+        print("No hay alguna luz más de la que cae de la luna a través del hueco de donde caíste.")
         time.sleep(1)
     elif elecc == "B":
         print("Miras el estado de tu ropa y cuerpo:\n"
-              "Tus piernas están algo heridas.\nTu cabeza recién se esta recuperando de la caída.\nToda tu espalda te duele y pesa por la carga que llevas detrás de ti")
+              "Tus piernas están algo heridas.\nTu cabeza recién se esta recuperando de la caída.\nToda tu espalda te duele y pesa por la carga que llevas detrás de ti, es una mochila")
+        mochilaOp = True
         time.sleep(1)
     elif elecc == "C":
         print("Es pesado y parece estar sostenido por tus hombros.\nEs una mochila y parece estar medio llena.")
@@ -99,8 +100,14 @@ time.sleep(1)
 
 if elecc == "A":
     print("Quieres moverte, pero tus piernas estan heridas y sientes un pequeño mareo")
+    print("Revisas la mochila en tu espalda")
+    print("Buscas dentro de la mochila y encuentras medicina, vendas, comida, agua, una pistola y dos cuchillos")
+
 elif elecc == "B":
     print("Estas en un hoyo y no observas un lugar para esconderte")
+    print("Revisas la mochila en tu espalda")
+    print("Buscas dentro de la mochila y encuentras medicina, vendas, comida, agua, una pistola y dos cuchillos")
+
 
 elif elecc == "C" and mochilaOp == False:
     print("Quieres salir del hoyo con tus propias manos pero resbalas y caes de nuevo")
@@ -115,6 +122,10 @@ elif elecc == "C" and mochilaOp == False:
 elif mochilaOp and elecc == "C":
     print("Buscas dentro de la mochila y encuentras medicina, vendas, comida, agua, una pistola y dos cuchillos")
 
+
+
+
+
 elecc = elecciones("Curarte las heridas y alimentarte con la que hay en la mochila", "Usar la pistola y disparar en la dirección de los ruidos", "Guardar silencio y esperar que se vayan los ruidos")
 inv = 0
 while elecc != "A" and elecc != "B" and elecc != "C":
@@ -128,6 +139,7 @@ if elecc == "A":
         print("Estas cuarándote, pero hiciste mucho ruido.Entonces escuchas que los rugidos se acercan a ti")
         print(" Tratas de correr, pero sientes un mordisco en la pierna y otro en la espalda, gritas")
         print("Sientes cómo se ta va la vida a mordiscos...")
+        dibujarMonstro()
         sys.exit()
 
 elif elecc == "B":
@@ -136,10 +148,9 @@ elif elecc == "B":
         print("Pero hiciste mucho ruido con el arma, más rugidos se acercan a ti...")
         print(" Tratas de correr disparando, pero sientes un mordisco en la pierna y otro en la espalda y gritas")
         print("Sientes cómo se ta va la vida a mordiscos...")
+        dibujarMonstro()
         sys.exit()
 elif elecc == "C":
         print("Eres paciente y esperas a que se vayan los ruidos")
         print("Entonces después de unos minutos estas sumergido en un profundo y agradable silencio sin rugidos estremecedores")  
-
-
 
